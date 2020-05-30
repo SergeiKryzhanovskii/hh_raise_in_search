984b7c0b60d16d5f8e14c51bcb7696b10e0cba8e
Sergei Kryzhanovskii <s.kryzhanovskii@gmail.com>
2020-05-30 15:50:33 +0300

    headless активна, запись логов есть

--- main.py
+++ main.py
@@ -12,17 +12,15 @@ import datetime
 
 def init_driver():
     """ Инициируется браузер через Selenium """
-    chromedriver = ('chromedriver.exe')  # путь к драйверу хром
-
-    ''' Если требуется запуск бе UI, то нужно раскомментировать "headless" '''
+    ch_driver = 'chromedriver.exe'
     options = webdriver.ChromeOptions()
     options.add_argument('headless')  # для открытия headless-браузера
-    driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+    driver = webdriver.Chrome(executable_path=ch_driver, chrome_options=options)
     return driver
 
 
 def login_hh():
-    """ Вход пользователя, данные вводятся логин и пароль вводятся после запуска"""
+    """ Вход пользователя, данные (логин и пароль) вводятся после запуска"""
     driver.get('https://hh.ru/applicant/resumes?from=header_new')
     time.sleep(2)
     try:
@@ -34,24 +32,28 @@ def login_hh():
         time.sleep(2)
     except TimeoutException:
         playsound('sounds/pomogite-spasite-sos.mp3')
-        print('Time Out', now.isoformat())
+        now_te = datetime.datetime.now()
+        time_te = now_te.isoformat()
+        with open('log.txt', 'a') as file:
+            file.write(f'Time Out: {time_te} \n')
         driver.quit()
         sys.exit()
 
 
 def update_time():
-    """ Попытка нажатия на кнопку поднятия резюме (1 раз в 4 часа)"""
+    """ Поднятие резюме (1 раз в 4 часа)"""
     try:
         driver.get('https://hh.ru/applicant/resumes?from=header_new')
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, "button[data-qa='resume-update-button']").click()
         time.sleep(2)
         playsound('sounds/nachinaju-dejstvovat-bez-shuma-i-pyli-po-vnov-utverzhennomu-planu.mp3')
-        print('session Ok')
     except ElementClickInterceptedException:
         playsound('sounds/pomogite-spasite-sos.mp3')
-        print('Кнопка неактивна (ElementClickInterceptedException), возможно не прошло время ожидания.',
-              now.isoformat())
+        now = datetime.datetime.now()
+        time_ec = now.isoformat()
+        with open('log.txt', 'a') as file:
+            file.write(f'Кнопка неактивна (ElementClickInterceptedException), {time_ec} \n')
         driver.quit()
         sys.exit()
 
@@ -60,12 +62,16 @@ if __name__ == '__main__':
     login = input('Please, enter phone or email: ').split()
     my_password = input('Please, enter password: ').split()
     now = datetime.datetime.now()
+    count = 1
     driver = init_driver()
     login_hh()
-    count = 0
-    for i in range(3):
-        update_time()
-        count += 1
-        print(f'Резюме поднято в поиске раз: {count}.', now.isoformat())
+    update_time()
+    print(f'CV update: {count}.', now.isoformat())
+    for i in range(100):
         time.sleep(14500)
+        now = datetime.datetime.now()
+        time_up = now.isoformat()
+        count += 1
+        with open('log.txt', 'a') as file:
+            file.write(f'CV update: {count}, {time_up} \n')
     driver.quit()
