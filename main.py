1cbc8b82a964ad004e2bc9cd20943cbcc255ce15
Sergei Kryzhanovskii <s.kryzhanovskii@gmail.com>
2020-06-04 00:40:43 +0300

    update function log and rename comments

--- main.py
+++ main.py
@@ -13,16 +13,23 @@ import getpass
 
 
 def init_driver():
-    """ Инициируется браузер через Selenium """
+    """ Emulate the user's browser """
     ch_driver = 'chromedriver.exe'
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')  # для открытия headless-браузера
+    #options.add_argument('headless')  # headless (browser starts without a graphical interface)
     driver = webdriver.Chrome(executable_path=ch_driver, chrome_options=options)
     return driver
 
 
+def logging(message):
+    now = datetime.datetime.now()
+    time_up = now.isoformat()
+    with open('log.txt', 'a') as file:
+        file.write(f'{message}: {time_up} \n')
+
+
 def login_hh():
-    """ Вход пользователя, данные (логин и пароль) вводятся после запуска"""
+    """ Log in """
     driver.get('https://hh.ru/applicant/resumes?from=header_new')
     time.sleep(2)
     try:
@@ -32,58 +39,48 @@ def login_hh():
         time.sleep(1.5)
         driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
         time.sleep(2)
+        message = 'Log in Ok'
+        logging(message)
     except TimeoutException:
-        playsound('sounds/pomogite-spasite-sos.mp3')
-        now_te = datetime.datetime.now()
-        time_te = now_te.isoformat()
-        with open('log.txt', 'a') as file:
-            file.write(f'Time Out: {time_te} \n')
+        # playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'Time Out'
+        logging(message)
         driver.quit()
         sys.exit()
 
 
 def update_time():
-    """ Поднятие резюме (1 раз в 4 часа)"""
+    """ Raise in search """
     try:
         driver.get('https://hh.ru/applicant/resumes?from=header_new')
         time.sleep(2)
         driver.find_element(By.CSS_SELECTOR, "button[data-qa='resume-update-button']").click()
         time.sleep(2)
-        playsound('sounds/nachinaju-dejstvovat-bez-shuma-i-pyli-po-vnov-utverzhennomu-planu.mp3')
+        #playsound('sounds/nachinaju-dejstvovat-bez-shuma-i-pyli-po-vnov-utverzhennomu-planu.mp3')  # fun for fun (read README.md)
+        message = 'CV update'
+        logging(message)
     except ElementClickInterceptedException:
-        playsound('sounds/pomogite-spasite-sos.mp3')
-        now = datetime.datetime.now()
-        time_ec = now.isoformat()
-        with open('log.txt', 'a') as file:
-            file.write(f'Element Click Intercepted Exception, {time_ec} \n')
+        #playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'Element Click Intercepted Exception'
+        logging(message)
         driver.quit()
         sys.exit()
     except NoSuchElementException:
-        playsound('sounds/pomogite-spasite-sos.mp3')
-        now = datetime.datetime.now()
-        time_ec = now.isoformat()
-        with open('log.txt', 'a') as file:
-            file.write(f'No Such Element Exception, {time_ec} \n')
+        #playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'No Such Element Exception'
+        logging(message)
         driver.quit()
         sys.exit()
 
 
 if __name__ == '__main__':
     login = input('Please, enter phone or email: ').split()
+    #my_password = input('Please, enter password: ').split() # read README.md ('Command line & PyCharm')
     my_password = getpass.getpass('Please, enter password: ').split()
-    now = datetime.datetime.now()
-    time_up = now.isoformat()
-    count = 1
     driver = init_driver()
     login_hh()
     update_time()
-    with open('log.txt', 'a') as file:
-        file.write(f'CV update: {count}, {time_up} \n')
     for i in range(100):
         time.sleep(14500)
-        now = datetime.datetime.now()
-        time_up = now.isoformat()
-        count += 1
-        with open('log.txt', 'a') as file:
-            file.write(f'CV update: {count}, {time_up} \n')
+        update_time()
     driver.quit()
