cbb5f7970459f7d23b0dd08ec9a83291a7360b7a
Sergei Kryzhanovskii <s.kryzhanovskii@gmail.com>
2020-07-27 03:48:17 +0300

    add main firefox, update README

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# hh_-raise_in_search
+# Сode for receiving user data from the [gosuslugi.ru](https://www.gosuslugi.ru/ "gosuslugi.ru")
+### Main stack
+- [Python](https://www.python.org/downloads/ "Python") v3.8.3;
+- [Selenium](https://www.selenium.dev/downloads/ "Selenium") v3.141;
+- [Webdrivers](https://www.selenium.dev/documentation/en/webdriver/driver_requirements/ "Webdrivers") for Selenium:
+	-  [Chromedriver ](https://chromedriver.storage.googleapis.com/index.html "Chromedriver ") v83.0.4103.39;
+	- [Geckodriver for Firefox ](https://github.com/mozilla/geckodriver/releases "Geckodriver for Firefox ") v0.26.0;
+- [beautifulsoup4](https://pypi.org/project/beautifulsoup4/ "beautifulsoup4") v4.9.1;
+- [Playsound](https://pypi.org/project/playsound/) v1.2.2;
+- json;
+- Windows 10.
+
+------------
+
+### Task:
+1. Emulate user browser;
+2. Raise your resume in search on the site [hh.ru](https://hh.ru/);
+3. Log the number of requests and their time.
+
+------------
+### Result
+1. Using Selenium to emulate browser and error handling;
+2. Create log file;
+3. Repeat every 4 hours.
+
+------------
+
+### Program feature
+#### Headless for Chrome
+In the repository, this option is commented out.
+This means that the browser starts with a graphical interface.
+To work in the background you need to uncomment this line:
+```python
+options.add_argument('headless')
+```
+#### Headless for Firefox
+In the repository, this option is commented out.
+This means that the browser starts with a graphical interface.
+To work in the background you need to uncomment this line:
+```python
+options.headless = True
+```
+
+------------
+
+### Command line & PyCharm
+When you run the program in command line - it's Ok.
+When you run the programm in PyCharm - she may stop at the password entry step. 
+This is a feature of the library`getpass`.
+To run in PyCharm you need to uncomment the line:
+```python
+my_password = input('Please, enter password: ').split()
+```
+and comment out the line:
+
+```python
+my_password = getpass.getpass('Please, enter password: ').split()
+```
+
+------------
+
+### Warning
+- A different version `chromedriver` and `geckodriver` of drivers may be required to emulate the browser (I attach links at the beginning).
+- The version depends on the operating system and build of your browser.
+- The repository comes with drivers for Windows 10.
+- It is important to indicate the correct path in the program to the driver files.
+
+------------
+
+### Just for fun 
+The program commented out the lines of the music file playback. If you uncomment them, the program will notify the user about the start of work and the occurrence of errors.
+She will notify with the help of audio fragments from the Soviet comedy film "Diamond Hand". #justforfun
---
+++ main_geckodrive.py
@@ -0,0 +1,87 @@
+# -*- python3/ utf-8 -*-
+
+import time
+import sys
+import datetime
+import getpass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
+from playsound import playsound
+
+
+
+def init_driver():
+    """ Emulate the user's browser """
+    firefoxdriver = ('geckodriver.exe')  # path for a geckodriver
+    options = Options()
+    # options.headless = True # headless (browser starts without a graphical interface)
+    driver = webdriver.Firefox(executable_path=firefoxdriver, firefox_options=options)
+    return driver
+
+def logging(message):
+    now = datetime.datetime.now()
+    time_up = now.isoformat()
+    with open('log.txt', 'a') as file:
+        file.write(f'{message}: {time_up} \n')
+
+
+def login_hh():
+    """ Log in """
+    driver.get('https://hh.ru/applicant/resumes?from=header_new')
+    time.sleep(2)
+    try:
+        driver.find_element(By.CLASS_NAME, "HH-AuthForm-Login").send_keys(login)
+        time.sleep(1.5)
+        driver.find_element(By.CLASS_NAME, "HH-AuthForm-Password").send_keys(my_password)
+        time.sleep(1.5)
+        driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+        time.sleep(2)
+        message = 'Log in Ok'
+        logging(message)
+    except TimeoutException:
+        # playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'Time Out'
+        logging(message)
+        driver.quit()
+        sys.exit()
+
+
+def update_time():
+    """ Raise in search """
+    try:
+        driver.get('https://hh.ru/applicant/resumes?from=header_new')
+        time.sleep(2)
+        driver.find_element(By.CSS_SELECTOR, "button[data-qa='resume-update-button']").click()
+        time.sleep(2)
+        #playsound('sounds/nachinaju-dejstvovat-bez-shuma-i-pyli-po-vnov-utverzhennomu-planu.mp3')  # fun for fun (read README.md)
+        message = 'CV update'
+        logging(message)
+    except ElementClickInterceptedException:
+        #playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'Element Click Intercepted Exception'
+        logging(message)
+        driver.quit()
+        sys.exit()
+    except NoSuchElementException:
+        #playsound('sounds/pomogite-spasite-sos.mp3') # fun for fun (read README.md)
+        message = 'No Such Element Exception'
+        logging(message)
+        driver.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    login = input('Please, enter phone or email: ').split()
+    #my_password = input('Please, enter password: ').split() # read README.md ('Command line & PyCharm')
+    my_password = getpass.getpass('Please, enter password: ').split()
+    driver = init_driver()
+    login_hh()
+    update_time()
+    for i in range(100):
+        time.sleep(14500)
+        update_time()
+    driver.quit()
